5337e22d8646e6ef8e2abf88db40efba5928d2c5
HITESH-JETHWANI02 <144786457+HITESH-JETHWANI02@users.noreply.github.com>
2024-05-06 11:45:22 +0530

    Add files via upload

---
+++ abstraction.py
@@ -0,0 +1,20 @@
+class name:
+    __age = 18
+
+ob = name
+print(ob.__age) 
+
+
+# class Bike:
+#     name = ""
+#     gear = 0
+
+# bk = Bike
+# # print(bk.gear)
+# # print(bk.name)
+
+# bk.name = "HONDA"
+# bk.gear = 4
+
+# print("BIKE:",bk.name,bk.gear)
+
